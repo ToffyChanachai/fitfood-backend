d42fcae754f6a4d4485ab2c7ff111c9d02ad90a2
ToffyChanachai <toffy.chanachai123@gmail.com>
2025-02-28 13:56:31 +0700

    update

--- server.js
+++ server.js
@@ -3,13 +3,14 @@
 const { Ignitor } = require('@adonisjs/ignitor')
 const http = require('http')
 
-new Ignitor(require('@adonisjs/fold'))
+// เริ่มต้น AdonisJS application
+const app = new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
-  .fireHttpServer()
-  .then(() => {
-    const port = process.env.PORT || 3333  // ใช้พอร์ตจากตัวแปรสภาพแวดล้อม
-    http.createServer(app.handle).listen(port, () => {
-      console.log(`Server is listening on port ${port}`)
-    })
-  })
-  .catch(console.error)
+  .make()
+
+const server = http.createServer(app.handle())
+
+const port = process.env.PORT || 3333
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+})
